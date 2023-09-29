aa77e1f7dcaca6da29ad8a379ce3898d562f8d0f
SirJain <94301223+SirJain0@users.noreply.github.com>
2023-09-29 18:48:04 -0400

    Port to 1.20.2, fix incorrect mod ID constant

--- src/main/java/com/sirjain/EzHealingMain.java
+++ src/main/java/com/sirjain/EzHealingMain.java
@@ -12,7 +12,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class EzHealingMain implements ModInitializer {
-	public static final String MOD_ID = "health_control";
+	public static final String MOD_ID = "ez_healing";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
